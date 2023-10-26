a2af4940311811d5abd7902825ba1ba1fea3598e
LuizFFarias <rm99519@fiap.com.br>
2023-10-26 10:55:20 -0300

    APIs locais e estilizaçao tailwind

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -5,7 +5,7 @@ export default function Cabecalho() {
   return (
     <header className='bg-gray-800 text-white h-16'>
     <nav className='flex justify-around'>
-      <Link className='link-cabecalho' href="/produtos/estaticas/tenis">TÊNIS</Link> 
+      <Link className='link-cabecalho' href="/consumo/eletro-view">ELETRÔNICOS</Link> 
       <Link className='bloc my-5' href="/produtos/dinamicas/estranho/tenis-preto">TÊNIS PRETO</Link> 
       <Link className='bloc my-5' href="/produtos/dinamicas/vestuario/camisa-xadrez">Camisa-X</Link>
     </nav>
